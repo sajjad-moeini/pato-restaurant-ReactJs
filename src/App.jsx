b5d56b4e4da1f84944f55ef053d011ff3c3b1b2f
sajjad <sajjadmoeini97@gmail.com>
2023-09-29 18:23:08 +0330

    init project and navbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarr from './Components/Navbar/Navbar'
+import { useRoutes } from 'react-router-dom';
+import routes from './router';
+import { useState } from 'react'
+
+
+function App() {
+const router = useRoutes(routes)
+
+  return (
+    <>
+      <Navbarr />
+      <div className='main-container'>
+
+      {router}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+export default function Navbarr() {
+  const [logoYPadding, setLogoYPadding] = useState(4)
+  const [bgColor, setBgColor] = useState('rgba(65, 64, 64, 0.616)')
+  const [navItemColor,setNavItemColor] =useState('#fff')
+  const [logoSrc,setLogoSrc]=useState("img/logo.png.webp")
+  window.document.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      setLogoYPadding(2)
+      setBgColor('rgba(255,255,255,0.8)')
+      setNavItemColor('#444')
+      setLogoSrc("img/logo2.png.webp")
+    } else {
+      setLogoYPadding(4)
+      setBgColor('rgba(65, 64, 64, 0.616)')
+      setNavItemColor('#fff')
+      setLogoSrc("img/logo.png.webp")
+    }
+  })
+  return (
+    <>
+      <Navbar expand="lg" id='navbar' style={{ background: bgColor }}>
+        <Container>
+          <Navbar.Brand href="#home" className={`navbar-logo-container py-${logoYPadding}`}>
+            <img src={logoSrc} id='logo' alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse id="main-navbar">
+            <Nav className="mx-auto">
+              <Nav.Link  >
+                <NavLink to={'/'} className='nav-item px-3' style={{color:navItemColor}} >HOME</NavLink>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <NavLink className='nav-item px-3' style={{color:navItemColor}} >MENU</NavLink>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <NavLink className='nav-item px-3' style={{color:navItemColor}} >RESERVATION</NavLink>
+              </Nav.Link>
+              <Nav.Link  href="#link">
+                <NavLink className='nav-item px-3' style={{color:navItemColor}}>GALLARY</NavLink>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <NavLink  className='nav-item px-3' style={{color:navItemColor}} >ABOUT</NavLink>
+              </Nav.Link>
+              <Nav.Link  href="#link">
+                <NavLink className='nav-item px-3' style={{color:navItemColor}}>BLOG</NavLink>
+              </Nav.Link>
+              <Nav.Link  href="#link">
+                <NavLink className='nav-item px-3' style={{color:navItemColor}}>CONTACT</NavLink>
+              </Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
