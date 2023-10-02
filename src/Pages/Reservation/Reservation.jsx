c2ba479288900ae336c127b8a1c32955723466c0
sajjad <sajjadmoeini97@gmail.com>
2023-10-02 16:22:08 +0330

    init reservation page

---
+++ src/Pages/Reservation/Reservation.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Reservation.css'
+export default function Reservation() {
+  return (
+    <div>Reservation</div>
+  )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home/Home";
 import Menu from "./Pages/Menu/Menu";
+import Reservation from "./Pages/Reservation/Reservation";
 
 const routes = [
        { path: '/' , element: <Home />},
        { path: '/menu' , element: <Menu />},
+       { path: '/reservation' , element: <Reservation/>},
 ]
 
 export default routes
