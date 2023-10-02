b9668431586eb06ca6343f0d36e0f0e46a3a80c8
sajjad <sajjadmoeini97@gmail.com>
2023-10-03 00:41:52 +0330

    update styles

--- src/Components/GallaryPageImageGen/GallaryPageImageGen.jsx
+++ src/Components/GallaryPageImageGen/GallaryPageImageGen.jsx
@@ -13,8 +13,11 @@ export default function GallaryPageImageGen({ type }) {
     <div className=' p-3 gallary-images-container'>
       <div className="row">
         {type.map((image, index) => (
-          <div className="col-12 col-md-4 mt-3 rounded-3 gallary-img-container position-relative"  data-aos="fade-left" key={index}>
-            <img src={`../${image}`} className='img-fluid gallary-img rounded-3 cp' data-aos="zoom-in-up" alt={`gallaryimg${index + 1}`} />
+          <div className="col-12 col-md-4 mt-3 rounded-3 gallary-img-container position-relative" data-aos="fade-left" key={index}>
+            <img src={`../${image}`} className='img-fluid gallary-img rounded-3 cp' data-aos="flip-left"
+              data-aos-offset="200"
+              data-aos-duration="1000"
+              data-aos-easing="ease-out-cubic" alt={`gallaryimg${index + 1}`} />
             <div className="gallary-img-layout position-absolute d-flex-centring ">
               <BiSearchAlt className='gallary-img-search-icon text-light fs-1' />
             </div>
